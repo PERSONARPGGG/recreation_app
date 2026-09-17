1fcdf08b6676c3e420ffb00eec6a9a966ce2cb66
DongHyunShin <personarpg7501@gmail.com>
2026-09-17 21:52:31 +0900

    build: compile project to docs folder for gh-pages root deployment

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,9 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   base: '/recreation_app/',
   plugins: [react()],
+  build: {
+    outDir: 'docs',
+  },
   server: {
     port: 3000,
     host: true
